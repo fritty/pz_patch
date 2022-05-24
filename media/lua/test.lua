449888596e14ad1bb23320a160af233383e18b4b
fritty <fritt@inbox.ru>
2022-05-24 20:32:41 +0300

    test

--- media/lua/test.lua
+++ media/lua/test.lua
@@ -4,7 +4,10 @@
 --- DateTime: 24.05.2022 18:42
 ---
 
-function myf()
+local function myf()
     local player = getSpecificPlayer(0)
     player:Say("Hello World")
+    print("I like trains!");
 end
+
+Events.EveryOneMinute.Add(myf);
